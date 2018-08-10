f48ce212b62717b132eb94aafc420d9007025045
Ben Roth <spongebucket@gmail.com>
2018-08-09 21:08:39 -0700

    wip save queue of history items

--- history.lua
+++ history.lua
@@ -1,30 +1,23 @@
 history = {
    _items = {},
    _numItems = 0,
-   MAX_HISTORY_LENGTH = 1, -- todo: increase this, make actual queue
+   MAX_HISTORY_LENGTH = 9, -- todo: increase this, make actual queue
 }
 
 function history:load()
-   local item = nil
+   self._items = {}
+   self._numItems = 0
    local historyFile = self:getFilePathAndCreateDirectory()
-   info = love.filesystem.getInfo(historyFile)
+   local info = love.filesystem.getInfo(historyFile)
    if info == nil then
-      -- print("no history exists")
+      -- print('no history file exists')
    else
-      local contents, size = love.filesystem.read(historyFile)
-      if size > 0 then
-         item = contents
-      else
-         -- print("unable to read file")
+      for line in love.filesystem.lines(historyFile) do
+         if line:len() > 0 then
+            self._numItems = self._numItems + 1
+            self._items[self._numItems] = line
+         end
       end
-   end
-
-   if item then
-      self._items = { item, }
-      self._numItems = 1
-   else
-      self._items = {}
-      self._numItems = 0
    end
 end
 
@@ -33,25 +26,49 @@ function history:get()
 end
 
 function history:push(item)
-   local historyFile = self:getFilePathAndCreateDirectory()
-   local success, message = love.filesystem.write(historyFile, item)
-   if success then
-      -- print("wrote scores file")
-      self._items = { item, }
-      self._numItems = 1
-   else
-      print("unable to write scores file:", message)
+   -- insert at beginning
+   -- (todo: efficient)
+   for index = self._numItems, 1, -1 do
+      self._items[index + 1] = self._items[index]
+   end
+   self._items[1] = item
+   self._numItems = self._numItems + 1
+
+   -- maybe trim
+   if self._numItems > self.MAX_HISTORY_LENGTH then
+      self._items[self._numItems] = nil
+      self._numItems = self._numItems - 1
+   end
+
+   -- save to disk
+   local success, message = self:write()
+   if not success then
+      print("unable to write history file:", message)
    end
 end
 
+function history:write()
+   local historyFile = self:getFilePathAndCreateDirectory()
+   local fileContents = ''
+   for index, item in pairs(self._items) do
+      print('trying to write item and index:', index, item)
+      if item:len() > 0 then
+         fileContents = fileContents .. item .. '\n'
+      end
+   end
+   return love.filesystem.write(historyFile, fileContents)
+end
+
 function history:getFilePathAndCreateDirectory()
+   -- TODO: nikki: fix this
    local saveDirectory = love.filesystem.getSaveDirectory()
+   -- local saveDirectory = '~/Library/Application Support/package'
    local historyDirectory = saveDirectory .. "/home/save"
    local info = love.filesystem.getInfo(historyDirectory)
    if info == nil then
       local success = love.filesystem.createDirectory(historyDirectory)
       if success == false then
-         print("unable to create scores save path")
+         print("unable to create history save path")
       end
    end
    local historyFilePath = historyDirectory .. "/history.dat"
